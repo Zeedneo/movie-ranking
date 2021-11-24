638f08474e919081ecf4a550133bf8e3ed06b922
hazelnope <nopnprii@gmail.com>
2021-11-24 22:12:17 +0700

    pagination, info button, api reFav, filter type

--- frontend/src/components/navbar/navBar.js
+++ frontend/src/components/navbar/navBar.js
@@ -1,8 +1,10 @@
 import navBar from '../navbar/navBar.css';
 import { Link, useNavigate } from "react-router-dom";
-import { BiMoviePlay } from 'react-icons/bi';
+import { BiMoviePlay, BiSearchAlt } from 'react-icons/bi';
 import { RiHeartsFill } from 'react-icons/ri';
 import { IoLogInOutline } from 'react-icons/io5';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 
 
@@ -10,12 +12,12 @@ function NavBar(props) {
     const navigate = useNavigate();
 
     return (
-        <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <nav className="navbar navbar-expand-md navbar-dark fixed-top bg">
             <div className="container-fluid">
+                <Link className="navbar-brand " to="/">
+                    <BiMoviePlay /> movie
+                </Link>
                 <div className="collapse navbar-collapse" id="navbarCollapse">
-                    <Link className="navbar-brand " to="/">
-                        <BiMoviePlay /> movie
-                    </Link>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
@@ -29,10 +31,22 @@ function NavBar(props) {
                             value={props.term}
                             onChange={event => {
                                 props.SearchKeyword(event.target.value);
-                                navigate(`../${event.target.value}`, { replace: false })
-
+                                navigate(`../${event.target.value}`, { replace: false });
+                                props.repage(1);
+                                props.type("");
                             }}
                         />
+                        <div class="dropdown">
+                            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                                Type
+                            </button>
+                            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                                <li><a class="dropdown-item" href="#" onClick={() => props.type("")}>all</a></li>
+                                <li><a class="dropdown-item" href="#" onClick={() => props.type("movie")}>movie</a></li>
+                                <li><a class="dropdown-item" href="#" onClick={() => props.type("series")}>series</a></li>
+                                <li><a class="dropdown-item" href="#" onClick={() => props.type("game")}>game</a></li>
+                            </ul>
+                        </div>
                     </form>
 
                     <ul className="navbar-nav ms-10 mb-2 mb-md-0">
--- frontend/src/components/pages/favorites/favorites.js
+++ frontend/src/components/pages/favorites/favorites.js
@@ -1,38 +1,111 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { BiBookHeart } from "react-icons/bi";
+import { ImInfo } from "react-icons/im";
+import { FaHeartBroken } from "react-icons/fa";
+import favorites from "../favorites/favorites.css"
 
 
 function Favorites(props) {
+    const [type, setType] = useState("");
     const [data, setData] = useState([]);
     const [click, setClick] = useState(0);
+    const [re, setRe] = useState(1);
+    const [infoId, setInfoId] = useState("");
+    const [nowId, setNowId] = useState(0);
 
-    useEffect(() => {
-        axios.post('http://3c2b-2001-fb1-0-703d-508-57c7-d135-65a1.ap.ngrok.io/showFavorite', {
-            username: props.Username
+    const removeFavorite = async (value) => {
+        await axios.post('http://f5c2-2001-fb1-0-703d-8029-4526-d6e8-6764.ap.ngrok.io/removeFavorite', {
+            username: props.Username, movieID: value
         })
             .then(function (response) {
+                setRe(0);
+                console.log(response.data.Response);
+                console.log(response.data.status);
+                console.log(response.data);
+            })
+    }
+
+    const checkInfo = (Id) => {
+        setInfoId(Id);
+    }
+
+    const checkshow = (Id) => {
+        if (infoId === Id) {
+            setNowId(1);
+        }
+    }
+
+    useEffect(() => {
+        axios.post('http://f5c2-2001-fb1-0-703d-8029-4526-d6e8-6764.ap.ngrok.io/showFavorite', {
+            username: props.Username, type: type
+        })
+            .then(function (response) {
+                // console.log("กด");
                 // console.log(response.data.Response)
                 // console.log(response.data.status)
                 // console.log(response.data.username)
                 // console.log(response.data.favorite)
-                setData(response.data.favorite)
+                setData(response.data.favorite);
+                console.log(response.data);
+                props.totalOfPages(1);
             })
-    }, [click]);
+    }, [click , re , type]);
 
 
     return (
         <div className="container">
-            {data.map((val, key) => {
-                return <div class="card" style={{ width: "16rem" }}>
-                    <div class="card-body">
-                        <img class="card-img-top" src={val.Poster} alt="..." />
-                        <h5 classname="card-title"> {val.Title}</h5>
-                        <h7 classname="card-type"> Type {val.Type} </h7>
-                        <br />
-                        <h7 classname="card-year"> Release Date {val.Year}</h7>
+            <div className="myFavAndType">
+                <h2 className="header title">My Favorites <BiBookHeart /></h2>
+                <div className="header">
+                    <div class="dropdown">
+                        <button class="btn btn-fav dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                            Type
+                        </button>
+                        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                            <li><a class="dropdown-item" href="#" onClick={() => setType("")}>all</a></li>
+                            <li><a class="dropdown-item" href="#" onClick={() => setType("movie")}>movie</a></li>
+                            <li><a class="dropdown-item" href="#" onClick={() => setType("series")}>series</a></li>
+                            <li><a class="dropdown-item" href="#" onClick={() => setType("game")}>game</a></li>
+                        </ul>
                     </div>
                 </div>
-            })}
+            </div>
+            <div className="row">
+                {data
+                    .map((val, key) => {
+                        return (
+                            // {
+                            //     nowId
+                            //     ? <div></div>
+                            //     :
+                            // }
+                            <div class="card">
+                                <div class="card-body">
+                                    <img class="card-img-top" src={val.Poster} alt="..." />
+                                    <h4 classname="card-title"> {val.Title}</h4>
+                                    <br />
+                                    <h6 classname="card-type"> Type {val.Type} </h6>
+                                    <h6 classname="card-year"> Release Date {val.Year}</h6>
+                                    <div>
+                                        <button
+                                            type="button"
+                                            classname="card-buttonInfo"
+                                            id="buttonInfo"
+                                        // onClick={() => { AddToFav(val.imdbID) }}
+                                        > <ImInfo /> </button>
+                                        <button
+                                            type="button"
+                                            classname="card-buttonUnFav"
+                                            id="buttonUnFav"
+                                            onClick={() => { removeFavorite(val.imdbID) }}
+                                        > <FaHeartBroken /> </button>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    })}
+            </div>
         </div>
     );
 }
